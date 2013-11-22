151ae855d22073615d4c8142c697fc92d86cbc79
Vladimir <vladimir@kerkhoff.nl>
2013-11-22 12:48:13 +0100

    Updated code format

--- app/code/community/Genmato/Payment/Helper/Data.php
+++ app/code/community/Genmato/Payment/Helper/Data.php
@@ -5,6 +5,7 @@
  * @copyright   Copyright (c) 2013 Genmato BV (http://genmato.com)
  */
 
-class Genmato_Payment_Helper_Data extends Mage_Core_Helper_Abstract {
+class Genmato_Payment_Helper_Data extends Mage_Core_Helper_Abstract
+{
 
 }
