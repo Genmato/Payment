c72be151af99b7440dd25ed7c5937d3946e04cc2
Vladimir Kerkhoff <v.kerkhoff@genmato.com>
2014-01-05 20:20:12 +0100

    Updated code format

--- app/code/community/Genmato/Payment/model/System/Invoice/Capture.php
+++ app/code/community/Genmato/Payment/model/System/Invoice/Capture.php
@@ -4,17 +4,19 @@
  * @package     Genmato_Payment
  * @copyright   Copyright (c) 2013 Genmato BV (http://genmato.com)
  */
- 
- class Genmato_Payment_Model_System_Invoice_Capture {
-	 protected $_options;
 
-	 public function toOptionArray() {
-		 if (!$this->_options) {
-			 $this->_options = array();
-			 $this->_options[] = array('value'=>'online','label'=>Mage::helper('sales')->__('Capture Online'));
-			 $this->_options[] = array('value'=>'offline','label'=>Mage::helper('sales')->__('Capture Offline'));
-			 $this->_options[] = array('value'=>'not_capture','label'=>Mage::helper('sales')->__('Not Capture'));
-		 }
-		 return $this->_options;
-	 }
- }
+class Genmato_Payment_Model_System_Invoice_Capture
+{
+    protected $_options;
+
+    public function toOptionArray()
+    {
+        if (!$this->_options) {
+            $this->_options = array();
+            $this->_options[] = array('value' => Mage_Sales_Model_Order_Invoice::CAPTURE_ONLINE, 'label' => Mage::helper('sales')->__('Capture Online'));
+            $this->_options[] = array('value' => Mage_Sales_Model_Order_Invoice::CAPTURE_OFFLINE, 'label' => Mage::helper('sales')->__('Capture Offline'));
+            $this->_options[] = array('value' => Mage_Sales_Model_Order_Invoice::NOT_CAPTURE, 'label' => Mage::helper('sales')->__('Not Capture'));
+        }
+        return $this->_options;
+    }
+}
